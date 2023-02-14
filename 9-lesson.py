a5893ef880eff0693e29048226bd15254d28f536
AmanUzaqbaev <uzaqbaevaman0@gmail.com>
2023-02-14 16:06:58 +0300

    Shift code

---
+++ 9-lesson.py
@@ -0,0 +1,24 @@
+import math
+'''print(math.pi)
+print(math.sqrt(625))#62**(1/2)
+
+print(pow(25,25))#625**25'''
+
+#округление
+'''print(round(23,14514413415154,2))#round(number,figure)'''
+
+print(math.ceil(23))
+print(math.floor(23.45))
+
+'''import random
+
+number = random.randint(0,100)
+pi = 3.14
+S = pi * number**2
+print(S)'''
+
+'''print(math.sqrt(441))'''
+'''print(pow(4,5))'''
+'''print(round(5.17985,2)) '''
+'''print(math.floor(5.17985))'''
+'''print(math.ceil(5.17985))'''
